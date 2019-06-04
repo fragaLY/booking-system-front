a3d1fdc3dc2a91039aab512a3517e653479db2b5
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-06-04 15:20:28 +0300

    React table is added.

--- src/pages/orders/index.jsx
+++ src/pages/orders/index.jsx
@@ -1,76 +1,137 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import DatePicker from 'react-datepicker';
+import ReactTable from 'react-table';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import 'react-table/react-table.css'
 import './index.css';
 
 import {
-  OrdersContainer,
+  DatePickerContainer,
   DatePickerWrapper,
-  OrdersTable,
-  OrderItem,
-  DatePickerContainer
+  OrdersContainer
 } from './styles';
 
 export class OrdersPage extends React.Component {
   state = {
     orders: [],
-    startDate: new Date(),
-    endDate: new Date()
+    startDate: Date.now(),
+    endDate: Date.now()
   }
 
   componentDidMount() {
     fetch('http://35.204.250.139:8080/api/orders')
-      .then(result => result.json())
-      .then(json => this.setState({ orders: json.orders }))
+        .then(result => result.json())
+        .then(json => this.setState({orders: json.orders}))
   };
 
   handleStartDateChange = (date) => {
-    this.setState({ startDate: date })
+    this.setState({startDate: date})
     console.log(date);
   }
 
   handleEndDateChange = (date) => {
-    this.setState({ endDate: date })
+    this.setState({endDate: date})
     console.log(date);
   }
 
   render() {
-    return (
-      <OrdersContainer>
-        <DatePickerContainer>
-          <DatePickerWrapper>
-            <DatePicker
-              dateFormat="yyyy-MM-dd"
-              selected={this.state.startDate}
-              onChange={this.handleStartDateChange}
-            />
-          </DatePickerWrapper>
-          <DatePickerWrapper>
-            <DatePicker
-              dateFormat="yyyy-MM-dd"
-              selected={this.state.endDate}
-              onChange={this.handleEndDateChange}
-            />
-          </DatePickerWrapper>
-        </DatePickerContainer>
 
-        <OrdersTable>
+    const columns = [
+      {
+        id: "Owner",
+        Header: 'Owner',
+        sortable: true,
+        filterable: true,
+        accessor: (order) => (<Link
+            to={'/api/users/' + order.owner.id}>{order.owner.lastName + " "
+        + order.owner.firstName}</Link>),
+        filterMethod: (filter, row) => {
+          return row[filter.id].props.children.startsWith(filter.value);
+        }
+      },
+      {
+        Header: 'Homes',
+        accessor: 'homes',
+        sortable: true,
+        filterable: true,
+        filterMethod: (filter, row) => {
+          filter.value = filter.value.replace(new RegExp("[^a-zA-Z]"), "");
+          return row[filter.id].toString().startsWith(filter.value);
+        }
+      },
+      {
+        Header: 'From',
+        accessor: 'from',
+        sortable: true,
+        filterable: true,
+        filterMethod: (filter, row) => {
+          filter.value = filter.value.replace(new RegExp("[^\\/0-9]"), "");
+          return row[filter.id].toString().startsWith(filter.value);
+        }
+      }, {
+        Header: 'To',
+        accessor: 'to',
+        sortable: true,
+        filterable: true,
+        filterMethod: (filter, row) => {
+          filter.value = filter.value.replace(new RegExp("[^\\/0-9]"), "");
+          return row[filter.id].toString().startsWith(filter.value);
+        }
+      }, {
+        Header: 'Cost',
+        accessor: 'cost',
+        sortable: true,
+        filterable: true,
+        filterMethod: (filter, row) => {
+          filter.value = filter.value.replace(new RegExp("[^a-zA-Z]"), "");
+          return row[filter.id].toString().startsWith(filter.value);
+        }
+      }, {
+        Header: 'Guests',
+        accessor: 'guests',
+        sortable: true,
+        filterable: true,
+        filterMethod: (filter, row) => {
+          filter.value = filter.value.replace(new RegExp("[^a-zA-Z]"), "");
+          return row[filter.id].toString().startsWith(filter.value);
+        }
+      }];
+
+    return (
+        <OrdersContainer>
+          <DatePickerContainer>
+            <DatePickerWrapper>
+              <DatePicker
+                  dateFormat="yyyy-MM-dd"
+                  selected={this.state.startDate}
+                  onChange={this.handleStartDateChange}
+              />
+            </DatePickerWrapper>
+            <DatePickerWrapper>
+              <DatePicker
+                  dateFormat="yyyy-MM-dd"
+                  selected={this.state.endDate}
+                  onChange={this.handleEndDateChange}
+              />
+            </DatePickerWrapper>
+          </DatePickerContainer>
+
           {
-            this.state.orders.map(order =>
-              <OrderItem key={order.id}>
-                <span>
-                  {order.owner.firstName}
-                </span>
-                &nbsp;
-                <span>
-                  {order.owner.lastName}
-                </span>
-              </OrderItem>
-            )
+            columns.map(<ReactTable
+                data={this.state.orders}
+                filterable={true}
+                columns={columns}
+                sortable={true}
+                defaultPageSize={10}
+                showPagination={true}
+                showPaginationBottom={true}
+                showPageJump={true}
+                className="-striped -highlight"
+            />)
           }
-        </OrdersTable>
-      </OrdersContainer>
+        </OrdersContainer>
     );
   }
 }
