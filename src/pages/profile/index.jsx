34a2c1657fae96f7e0cdff5e6001a0e0e6459049
dmitryRomankov <strmr@tut.by>
2019-06-06 19:14:42 +0300

    implemented loading user on the server

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -4,7 +4,6 @@ import 'react-table/react-table.css'
 import '../common/index.css';
 
 export class UserPage extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -19,39 +18,36 @@ export class UserPage extends React.Component {
       hasError: false,
       error: ''
     };
-
-    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-
-    const {profileUrl} = this.props.location.state;
+    const { profileUrl } = this.props.location.state;
 
     fetch(profileUrl)
-        .then((response) => {
-          if (response.status === 200) {
-            return response.json().then((json) => {
-              this.setState({
-                firstName: json.firstName,
-                lastName: json.lastName,
-                email: json.email,
-                phone: json.phone,
-                country: json.country,
-                city: json.city,
-                currency: json.currency,
-                registered: json.registered,
-                hasError: false,
-                error: ''
-              });
-            });
-          } else {
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json().then((json) => {
             this.setState({
-              hasError: true,
-              error: response.json.message
-            })
-          }
-        })
-        .catch(error => console.error("Error:", error))
+              firstName: json.firstName,
+              lastName: json.lastName,
+              email: json.email,
+              phone: json.phone,
+              country: json.country,
+              city: json.city,
+              currency: json.currency,
+              registered: json.registered,
+              hasError: false,
+              error: ''
+            });
+          });
+        } else {
+          this.setState({
+            hasError: true,
+            error: response.json.message
+          })
+        }
+      })
+      .catch(error => console.error("Error:", error))
   }
 
   componentDidCatch(error, message) {
@@ -62,132 +58,170 @@ export class UserPage extends React.Component {
     console.error(error, message);
   }
 
-  handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
+  handleFirstNameChange = (e) => {
+    this.setState({ firstName: e.target.value })
   }
 
-  handleSubmit() {
+  handleLastNameChange = (e) => {
+    this.setState({ lastName: e.target.value })
+  }
 
-    const {profileUrl} = this.props.location.state;
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value })
+  }
 
+  handlePhoneChange = (e) => {
+    this.setState({ phone: e.target.value })
+  }
+
+  handleCountryChange = (e) => {
+    this.setState({ country: e.target.value })
+  }
+
+  handleCityChange = (e) => {
+    this.setState({ city: e.target.value })
+  }
+
+  handleCurrencyChange = (e) => {
+    this.setState({ currency: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      phone: this.state.email,
+      country: this.state.country,
+      city: this.state.city,
+      currency: this.state.country,
+      registered: this.state.registered,
+    };
+
+    this.loadUserOnServer(user);
+  }
+
+  loadUserOnServer = (user) => {
+    const { profileUrl } = this.props.location.state;
     fetch(profileUrl, {
-      method: 'put',
+      method: 'PUT',
       headers: {
         "Content-type": "application/json"
       },
-      body: `{"firstName":${this.state.firstName},
-              "lastName":${this.state.lastName},
-              "email":${this.state.email},
-              "phone":${this.state.phone},
-              "country":${this.state.country},
-              "city":${this.state.city},
-              "currency":${this.state.currency}`
-    }).then((response) => {
-
-      if (response.status !== 204) {
-        return response.json().then((json) => {
-          this.setState({
-            hasError: true,
-            error: json.message
+      body: `{"firstName":${user.firstName},
+              "lastName":${user.lastName},
+              "email":${user.email},
+              "phone":${user.phone},
+              "country":${user.country},
+              "city":${user.city},
+              "currency":${user.currency}`
+    })
+      .then((response) => {
+        if (response.status !== 204) {
+          return response.json().then((json) => {
+            this.setState({
+              hasError: true,
+              error: json.message
+            });
           });
-        });
-      } else {
-        this.setState({
-          hasError: false,
-          error: ''
-        });
-      }
-
-    }).catch(error => {
-      console.error('Error:', error);
-    });
+        } else {
+          this.setState({
+            hasError: false,
+            error: ''
+          });
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 
   render() {
-
     return (
-        <div className="user">
-          <form onSubmit={this.handleSubmit}>
-            {this.state.hasError ? this.state.error : null}
-            <label>
-              Lastname:
+      <div className="user">
+        <form onSubmit={this.handleSubmit}>
+          {this.state.hasError ? this.state.error : null}
+          <label>
+            Lastname:
               <input
-                  type="text"
-                  name="lastName"
-                  value={this.state.lastName}
-                  onChange={this.handleChange}/>
-            </label>
-            <br/>
-            <label>
-              Firstname:
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleLastNameChange} />
+          </label>
+          <br />
+          <label>
+            Firstname:
               <input
-                  type="text"
-                  name="firstName"
-                  value={this.state.firstName}
-                  onChange={this.handleChange}/>
-            </label>
-            <br/>
-            <label>
-              Email:
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleFirstNameChange} />
+          </label>
+          <br />
+          <label>
+            Email:
               <input
-                  type="email"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleChange}/>
-            </label>
-            <br/>
-            <label>
-              Phone:
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleEmailChange} />
+          </label>
+          <br />
+          <label>
+            Phone:
               <input
-                  type="tel"
-                  name="phone"
-                  value={this.state.phone}
-                  onChange={this.handleChange}/>
-            </label>
-            <br/>
-            <label>
-              Country:
+              type="tel"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handlePhoneChange} />
+          </label>
+          <br />
+          <label>
+            Country:
               <input
-                  type="text"
-                  name="country"
-                  value={this.state.country}
-                  onChange={this.handleChange}/>
-            </label>
-            <br/>
-            <label>
-              City:
+              type="text"
+              name="country"
+              value={this.state.country}
+              onChange={this.handleCountryChange} />
+          </label>
+          <br />
+          <label>
+            City:
               <input
-                  type="text"
-                  name="city"
-                  value={this.state.city}
-                  onChange={this.handleChange}/>
-            </label>
-            <br/>
-            <label>
-              Currency:
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleCityChange} />
+          </label>
+          <br />
+          <label>
+            Currency:
               <select name="currency"
-                      value={this.state.currency}
-                      onChange={this.handleChange}>
-                <option value="BYN">BYN</option>
-                <option value="RUB">RUB</option>
-                <option value="USD">USD</option>
-                <option value="EUR">EUR</option>
-              </select>
-            </label>
-            <br/>
-            <label>
-              Registered:
+              value={this.state.currency}
+              onChange={this.handleCurrencyChange}>
+              <option value="BYN">BYN</option>
+              <option value="RUB">RUB</option>
+              <option value="USD">USD</option>
+              <option value="EUR">EUR</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Registered:
               <input
-                  type="text"
-                  name="registered"
-                  value={this.state.registered}
-                  disabled/>
-            </label>
-            <br/>
-
-            <input type="submit" value="Save"/>
-          </form>
-        </div>
+              type="text"
+              name="registered"
+              value={this.state.registered}
+              disabled
+            />
+          </label>
+          <br />
+          <input type="submit" value="Save" />
+        </form>
+      </div>
     )
   }
 }
