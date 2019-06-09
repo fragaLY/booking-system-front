411ecc809f3346c00be0aa309a61ff10dfafe6d2
fragaly <fragalymlg@gmail.com>
2019-06-09 17:50:00 +0300

    Footer updated

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,7 +6,9 @@ export class Footer extends React.Component {
     render() {
         return (
             <FooterWrapper>
-                Booking System
+                <a href="https://vk.com/belyptah" target="_blank">
+                    <img src="http://pngimg.com/uploads/vkontakte/vkontakte_PNG19.png" width="5%" height="auto" alt="Null"/>
+                </a>
             </FooterWrapper>
         );
     }
