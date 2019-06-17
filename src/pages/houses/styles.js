8a4558a20be1a3e459363d629790c94e7e771e1e
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-06-17 19:25:25 +0300

    Houses style changed.

--- src/pages/houses/styles.js
+++ src/pages/houses/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const GalleryWrapper = styled.div`
   height: 100%;
-  width: 50%;
+  width: 100%;
   z-index: 1;
   top: 0;
   overflow-x: hidden;
@@ -10,7 +10,13 @@ export const GalleryWrapper = styled.div`
 ;
 
 export const BrickGalleryWrapper = styled.div`
+  float: left;
+  width: 50%;
+  margin: 1rem 1rem 1rem 1rem;
 `;
 
 export const WoodenGalleryWrapper = styled.div`
+  float: right;
+  width: 50%;
+  margin: 1rem 1rem 1rem 1rem;
 `;
