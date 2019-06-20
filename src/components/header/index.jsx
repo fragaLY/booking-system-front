aecfaf3793ae240f508f282a55b830dbac570d2a
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-06-20 17:24:34 +0300

    Login image

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,36 +1,30 @@
 import React from 'react';
 
-import {
-  HeaderWrapper,
-  Logo,
-  RoutesWrapper,
-  NavItem
-} from './styles';
-import { routes } from '../../asserts/routes';
+import {HeaderWrapper, Logo, NavItem, RoutesWrapper} from './styles';
+import {routes} from '../../asserts/routes';
+import NavLink from "react-bootstrap/NavLink";
+import Image from "react-bootstrap/Image";
 
 export class Header extends React.Component {
   render() {
     return (
-      <HeaderWrapper>
-        <Logo
-          as={NavItem} to='/'
-          style={{ color: 'black', fontSize: '2.4rem' }}
-        >
-          Bely Ptah
-        </Logo>
-        <RoutesWrapper>
-          {
-            routes.map(route =>
-              <NavItem
-                to={route.to}
-                children={route.text}
-                key={route.text}
-                activeStyle={{ color: '#0071ba', backgroundColor: '#fafafa' }}
-              />
-            )
-          }
-        </RoutesWrapper>
-      </HeaderWrapper>
+        <HeaderWrapper>
+          <Logo
+              as={NavItem} to='/'
+              style={{color: 'black', fontSize: '2.4rem'}}
+          >
+            Bely Ptah
+          </Logo>
+          <RoutesWrapper>
+            {
+              routes.map(route =>
+                  route.text === 'Login'
+                      ? <Image as={NavLink('/login')} src={'./login.png'} to={route.to} key={route.text} activestyle={{ color: '#0071ba', backgroundColor: '#fafafa' }} roundedCircle/>
+                      : <NavItem to={route.to} children={route.text} key={route.text} activestyle={{ color: '#0071ba', backgroundColor: '#fafafa' }}/>
+              )
+            }
+          </RoutesWrapper>
+        </HeaderWrapper>
     );
   }
 }
